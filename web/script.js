470bf38bd95dbaaeea48b690a3828a9011801840
mqxmm <79595418+mqxmm@users.noreply.github.com>
2022-08-28 18:43:27 +0300

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,43 +1,43 @@
-let btngo = document.querySelector("#btngo"); //Переменная btngo прикрепляется к html объекту btngo
-//btngo.addEventListener("click", fun); //Добавить евент к btngo чтобы по клику делать fun
+let btngo = document.querySelector("#btngo");
+//btngo.addEventListener("click", fun);
 
 
      let ipatk = document.querySelector("#ipatk").value;
      let portatk = document.querySelector("#portatk").value;
 
-//Функция fun где в переменные ipatk и portatk закидывается значение из html ipatk и portatk
+
 function fun() {
      let ddl = document.getElementById("selectsystem");
      let selectedValue = ddl.options[ddl.selectedIndex].value;
     if (selectedValue == "viberite") {
-        alert("Пожалуйста, выберите тип атаки из списка");
+        alert("Please, choose the type of attack");
     }
    else if (selectedValue == "xp")
    {
    eel.winxp(ipatk,portatk);
-   alert("Атака началась. Не закрывайте окно терминала.");
+   alert("Attack is started. Dont close the terminal.");
    }
 
    else if (selectedValue == "w7")
    {
    eel.w7(ipatk,portatk);
-   alert("Атака началась. Не закрывайте окно терминала.");
+   alert("Attack is started. Dont close the terminal.");
    }
 
    else if (selectedValue == "w10")
    {
    eel.w10(ipatk,portatk);
-   alert("Атака началась. Не закрывайте окно терминала.");
+   alert("Attack is started. Dont close the terminal.");
    }
     else if (selectedValue == "dos")
    {
    eel.dos(ipatk,portatk);
-   alert("Атака началась. Не закрывайте окно терминала.");
+   alert("Attack is started. Dont close the terminal.");
    }
   else  if (selectedValue == "scan")
    {
    eel.scan(ipatk,portatk);
-   alert("Атака началась. Не закрывайте окно терминала.");
+   alert("Attack is started. Dont close the terminal.");
    }
 }
 
